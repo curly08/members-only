1d2cffac151fc301cfe9ef268f90c6f5be0d2e51
curly08 <mattynoakes08@gmail.com>
2021-12-03 16:10:14 -0500

    Relate Posts and Users, add controller filters, add Registrations controller and edit user_params, markup views based on sign-in status

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_03_180629) do
+ActiveRecord::Schema.define(version: 2021_12_03_201818) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
